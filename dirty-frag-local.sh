c67deb8284cc75ba6bbff89b06909b03012843e6
Carlos Rodríguez <crodriguezbrito@gmail.com>
2026-05-18 16:10:50 +0100

    prevent to execute the script if the modules are not loaded

--- dirty-frag-local.sh
+++ dirty-frag-local.sh
@@ -3,37 +3,48 @@
 
 #!/bin/bash
 
+# Asegurar que el script se ejecute como root
+if [ "$EUID" -ne 0 ]; then
+  echo "Por favor, ejecuta este script como root o usando sudo."
+  exit 1
+fi
+
 echo "==============================================="
 CURRENT_HOST=$(hostname)
 echo "Iniciando dirty frag en: $CURRENT_HOST" 
 
-CONF_FILE="/etc/modprobe.d/dirty-frag.conf"
+if lsmod | grep -E "esp4|esp6|rxrpc" >/dev/null; then
 
-cat > $CONF_FILE << 'EOF'
+    CONF_FILE="/etc/modprobe.d/dirty-frag.conf"
+
+    cat > $CONF_FILE << 'EOF'
 install esp4 /bin/false
 install esp6 /bin/false
 install rxrpc /bin/false
 EOF
 
-if [ -f "/etc/modprobe.d/dirty-frag.conf" ]; then
-    echo "Archivo creado con éxito."
+    if [ -f "/etc/modprobe.d/dirty-frag.conf" ]; then
+        echo "Archivo creado con éxito."
+    else
+        echo "El archivo no existe."
+    fi
+
+    echo "Actualizando initramfs..."
+    sudo update-initramfs -u -k all
+
+    echo "Intentando descargar módulos en ejecución..."
+    sudo rmmod esp4 2>/dev/null || true
+    sudo rmmod esp6 2>/dev/null || true
+    sudo rmmod rxrpc 2>/dev/null || true
+
+    echo "Verificación:"
+    if lsmod | grep -E "esp4|esp6|rxrpc" >/dev/null; then
+        echo "Algunos módulos siguen activos"
+    else
+        echo "No hay módulos vulnerables cargados"
+    fi
 else
-    echo "El archivo no existe."
-fi
-
-echo "Actualizando initramfs..."
-sudo update-initramfs -u -k all
-
-echo "Intentando descargar módulos en ejecución..."
-sudo rmmod esp4 2>/dev/null || true
-sudo rmmod esp6 2>/dev/null || true
-sudo rmmod rxrpc 2>/dev/null || true
-
-echo "Verificación:"
-if lsmod | grep -E "esp4|esp6|rxrpc" >/dev/null; then
-    echo "Algunos módulos siguen activos"
-else
-    echo "No hay módulos vulnerables cargados"
+    echo "Los módulos vulnerables no están cargados"
 fi
 
 echo "Proceso terminado para $CURRENT_HOST"
